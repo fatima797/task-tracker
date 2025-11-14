fd912786248935f02216ca0b45eb8f14a5ee48f6
Fatima Abubeker <132840772+fatima797@users.noreply.github.com>
2025-11-14 12:22:23 -0500

    test(task): add unit tests for Task class and update Task behaviour

--- src/com/github/fatima797/tasktracker/model/Task.java
+++ src/com/github/fatima797/tasktracker/model/Task.java
@@ -47,6 +47,7 @@ public class Task {
 
 	public void setDescription(String description) {
 		this.description = description;
+		updateTimestamp();
 	}
 
 
---
+++ test/com/github/fatima797/tasktracker/model/TaskTest.java
@@ -0,0 +1,49 @@
+package com.github.fatima797.tasktracker.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class TaskTest {
+	
+	@Test
+	void constructorShouldSetAllFieldsCorrectly() {
+		LocalDateTime now = LocalDateTime.now();
+		
+		Task task = new Task(1, "Learn JUnit", Status.IN_PROGRESS, now, now);
+		
+		assertEquals(1, task.getId());
+		assertEquals("Learn JUnit", task.getDescription());
+		assertEquals(Status.IN_PROGRESS, task.getStatus());
+		assertEquals(now, task.getCreatedAt());
+		assertEquals(now, task.getUpdatedAt());
+	
+	}
+	
+	@Test
+	void testSetDescription_updateFieldCorrectly() {
+		String initialDesc = "Old desc";
+		LocalDateTime initialUpdateTime = LocalDateTime.now().minusSeconds(1);
+		String newDesc = "New desc";
+		
+		Task task = new Task(1, initialDesc, Status.TODO, initialUpdateTime, initialUpdateTime);
+		
+		task.setDescription(newDesc);
+		
+		assertEquals(newDesc, task.getDescription());
+	}
+	
+	@Test
+	void testUpdateTimestampEffect_afterSetDescription() {
+		LocalDateTime initialUpdateTime = LocalDateTime.now().minusSeconds(1); 
+	    Task task = new Task(1, "Desc", Status.TODO, initialUpdateTime, initialUpdateTime);
+	    
+	    task.setDescription("New Desc");
+	    
+	    assertTrue(task.getUpdatedAt().isAfter(initialUpdateTime), "The updatedAt timestamp must be newer than the initial time");
+	}
+
+}
